c5487b6ec1dcda0afdb11ecc1bb02b3670ea94a4
Patrykos109 <pat.wawrzonek@gmail.com>
2024-03-10 13:41:25 +0100

    Add login page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
 import Login from './Login'
+import { BrowserRouter,Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
-    <Login/>
-  );
+    <BrowserRouter>
+        <Routes>
+            <Route path='/' element={<Login/>}></Route>
+            
+        </Routes>
+    </BrowserRouter>
+
+  )
 }
 
 export default App;
--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -1,31 +1,26 @@
-import React from 'react'
+import React from 'react';
 
 function Login() {
-
-    return(
-        <div className='d-felx justify-content-center align-items-center bg-primary vh-100'>
+    return (
+        <div className='d-flex justify-content-center align-items-center bg-primary vh-100'>
             <div className='bg-white p-3 rounded w-25'>
                 <form action="">
                     <div className='mb-3'>
-                    <label htmlFor="email">Email</label>
-                    <input type="email" placeholder='Enter Email'/>    
+                        <label htmlFor="email"><strong>Email</strong></label>
+                        <input type="email" placeholder='Enter Email' className='form-control rounded-0'/>    
                     </div>
 
                     <div className='mb-3'>
-                    <label htmlFor="password">Password</label>
-                    <input type="password" placeholder='Enter Password'/>    
+                        <label htmlFor="password"><strong>Password</strong></label>
+                        <input type="password" placeholder='Enter Password' className='form-control rounded-0' />    
                     </div>
-                    <button className='btn btn-success'>Log in</button>
+                    <button className='btn btn-success w-100 rounded-0'><strong>Log in</strong></button>
                     <p>Agree to your terms and policies</p>
-                    <button className='btn btn-default border'>Create Account</button>
+                    <button className='btn btn-default border w-100 rounded-0'>Create Account</button>
                 </form>
-
-
             </div>
         </div>
-
-
-
-    )
+    );
 }
-export default Login
+
+export default Login;
