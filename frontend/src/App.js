2e8651a87e8c9f7752256305cdccc7f47cdb762c
Patrykos109 <pat.wawrzonek@gmail.com>
2024-03-11 11:59:21 +0100

    Login page without database

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
 import Login from './Login'
 import { BrowserRouter,Routes, Route } from 'react-router-dom'
+import Signup from './Signup'
 
 function App() {
   return (
     <BrowserRouter>
         <Routes>
             <Route path='/' element={<Login/>}></Route>
-            
+            <Route path='/signup' element={<Signup/>}></Route>
         </Routes>
     </BrowserRouter>
 
--- frontend/src/Signup.js
+++ frontend/src/Signup.js
@@ -1,9 +1,11 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import Validation from './LoginValidation';
+import React ,{useState} from "react";
+import { Link } from "react-router-dom";
+import Validation from "./SignupValidation";
 
-function Login() {
+function Signup() {
+
     const [values, setValues] = useState({
+        name: '',
         email: '',
         password: ''
     })
@@ -12,7 +14,7 @@ function Login() {
     const handleInput = (event) => {
         setValues(prev => ({ ...prev, [event.target.name]: event.target.value }))
     }
-    
+
     const handleSubmit = (event) => {
         event.preventDefault();
         setErrors(Validation(values));
@@ -20,32 +22,45 @@ function Login() {
 
 
 
+
+
+
+
+
     return (
+
         <div className='d-flex justify-content-center align-items-center bg-primary vh-100'>
+
             <div className='bg-white p-3 rounded w-25'>
-                <h2>Sign-in</h2>
+                <h2>Sign-up</h2>
                 <form action="" onSubmit={handleSubmit}>
+                    <div className='mb-3'>
+                        <label htmlFor="name"><strong>Name</strong></label>
+                        <input type="text" placeholder='Enter Name' name='name'
+                            onChange={handleInput} className='form-control rounded-0' />
+                            {errors.name && <span className='text-danger'>{errors.name}</span>}
+                    </div>
                     <div className='mb-3'>
                         <label htmlFor="email"><strong>Email</strong></label>
                         <input type="email" placeholder='Enter Email' name='email'
                             onChange={handleInput} className='form-control rounded-0' />
-                        {errors.email && <span className='text-danger'>{errors.email}</span>}
+                            {errors.email && <span className='text-danger'>{errors.email}</span>}
                     </div>
 
                     <div className='mb-3'>
                         <label htmlFor="password"><strong>Password</strong></label>
                         <input type="password" placeholder='Enter Password' name='password'
                             onChange={handleInput} className='form-control rounded-0' />
-                        {errors.password && <span className='text-danger'>{errors.password}</span>}
+                            {errors.password && <span className='text-danger'>{errors.password}</span>}
                     </div>
-                    <button type='submit' className='btn btn-success w-100 rounded-0'><strong>Log in</strong></button>
+                    <button type='submit' className='btn btn-success w-100 rounded-0'><strong>Sign up</strong></button>
                     <p>Agree to your terms and policies</p>
-                    <Link to="/signup" className='btn btn-default border w-100 rounded-0 text-decoration-none'>Create Account</Link>
+                    <Link to="/" className='btn btn-default border w-100 rounded-0 text-decoration-none'>Login</Link>
 
                 </form>
             </div>
         </div>
-    );
+    )
 }
 
-export default Login;
+export default Signup;
