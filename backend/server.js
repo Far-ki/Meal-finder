f3fc8510e36d5981e58930842eaf0035d398463f
Patrykos109 <pat.wawrzonek@gmail.com>
2024-03-14 09:31:12 +0100

    Working registration and login

--- backend/server.js
+++ backend/server.js
@@ -30,6 +30,22 @@ app.post('/signup', (req, res) => {
 
 })
 
+app.post('/login', (req, res) => {
+    const sql = "SELECT * FROM login WHERE email = ? AND password = ?";
+    db.query(sql, [req.body.email,req.body.password], (err, data) => {
+        if (err) {
+            return res.json("Error");
+        }
+        if(data.length > 0){
+            return res.json("Success");
+        }
+        else{
+            return res.json("Faile")
+        }
+    })
+
+})
+
 app.listen(8081, () => {
     console.log("listening");
 })
