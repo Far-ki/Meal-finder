8796efec71c8d7a877616ffae1b6676b0bdcdcb7
Patrykos109 <pat.wawrzonek@gmail.com>
2024-03-16 19:13:10 +0100

    scrap from gotujmy.pl without database

---
+++ backend/scrap.js
@@ -0,0 +1,59 @@
+const axios = require('axios');
+const cheerio = require('cheerio');
+
+async function scrapeRecipes(url) {
+    try {
+        let page = 0;
+        while (true) {
+            const response = await axios.get(`${url}?offset=${page * 13}`);
+            const html = response.data;
+            const $ = cheerio.load(html);
+
+            console.log(`Scraping page: ${response.config.url} \n`);
+
+            const recipes = $('div.tile');
+
+            if (recipes.length === 0) {
+                console.log('No more recipes found.');
+                break;
+            }
+
+            for (let i = 0; i < recipes.length; i++) {
+                const recipeElement = recipes[i];
+                try {
+                    const recipeUrl = $(recipeElement).find('a.tile-inner').attr('href');
+
+                    if (recipeUrl !== undefined) {
+                        const recipeResponse = await axios.get(recipeUrl);
+                        const recipeHtml = recipeResponse.data;
+                        const $recipe = cheerio.load(recipeHtml);
+
+                        const recipeTitle = $recipe('h1').text();
+
+                        const ingredients = [];
+                        $recipe('li[itemprop="recipeIngredient"]').each((index, element) => {
+                            let ingredient = $(element).text().trim();
+                            if (ingredient.endsWith(',')) {
+                                ingredient = ingredient.slice(0, -1);
+                            }
+                            ingredients.push(ingredient);
+                        });
+
+                        console.log(`Title: ${recipeTitle}`);
+                        console.log('Ingredients:', ingredients);
+                    }
+                } catch (error) {
+                    console.error(`Error while scraping recipe data: ${error}`);
+                }
+            }
+
+            page++;
+            await new Promise(resolve => setTimeout(resolve, 5000));
+        }
+    } catch (error) {
+        console.error('Error while scraping the page:', error);
+    }
+}
+
+const mainPageUrl = 'https://gotujmy.pl/przepisy.html';
+scrapeRecipes(mainPageUrl);
